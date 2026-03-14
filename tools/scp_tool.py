1c4075270867fbbf76e3779fbcc9502d263d7bdd
Siddhakam Palmal <sidpalmal@gmail.com>
2026-03-14 19:22:44 +0530

    Implement SCP file transfer tool

---
+++ tools/scp_tool.py
@@ -0,0 +1,27 @@
+from core.utils import run_command
+
+
+class ScpTool:
+
+    name = "scp"
+
+    def transfer(self, src, dst, conn, bandwidth=None, direction="push"):
+
+        cmd = ["scp"]
+
+        if conn["key"]:
+            cmd += ["-i", conn["key"]]
+
+        if direction == "push":
+
+            source = src
+            dest = f"{conn['user']}@{conn['host']}:{dst}"
+
+        else:
+
+            source = f"{conn['user']}@{conn['host']}:{src}"
+            dest = dst
+
+        cmd += [source, dest]
+
+        run_command(cmd)
