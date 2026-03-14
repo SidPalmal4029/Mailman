0d4851e4f15d37a5b6d5a75fe8fdc6dc333d234e
Siddhakam Palmal <sidpalmal@gmail.com>
2026-03-14 19:31:16 +0530

    Refactor RsyncTool class and transfer method

--- tools/rsync_tool.py
+++ tools/rsync_tool.py
@@ -1,20 +1,14 @@
 from core.utils import run_command
 
-
 class RsyncTool:
     """
     Transfer implementation using rsync
     """
-
     name = "rsync"
-
     def transfer(self, src, dst, conn, bandwidth=None, direction="push"):
-
         ssh_cmd = "ssh"
-
         if conn["key"]:
             ssh_cmd += f" -i {conn['key']}"
-
         cmd = [
             "rsync",
             "-av",
@@ -28,20 +22,13 @@ class RsyncTool:
         # Optional bandwidth limit
         if bandwidth:
             cmd += ["--bwlimit", str(bandwidth)]
-
         if direction == "push":
-
             source = src
             destination = f"{conn['user']}@{conn['host']}:{dst}"
-
         elif direction == "pull":
-
             source = f"{conn['user']}@{conn['host']}:{src}"
             destination = dst
-
         else:
             raise ValueError("direction must be push or pull")
-
         cmd += [source, destination]
-
         run_command(cmd)
